d4eac60740ee040422d8eb2aec725e09f99c764a
Koushik Saha <koushik.saha666@gmail.com>
2021-12-03 12:41:37 +0600

    add double slider login to next js blog ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/prismaticforms.scss'
 import '../styles/scrolllogin.scss'
+import '../styles/doubleslider.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
